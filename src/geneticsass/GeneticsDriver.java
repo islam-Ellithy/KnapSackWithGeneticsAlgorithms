c87599149ee908f20ce04ace50975588bd19a3fa
islam.emmo <islam.emmo@gmail.com>
2017-10-15 20:04:18 +0200

    second commit

---
+++ src/geneticsass/GeneticsDriver.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geneticsass;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MrHacker
+ */
+public class GeneticsDriver implements Modification{
+    
+    @Override
+    public int fitnessFunction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<PairOfChromosome> selection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<PairOfChromosome> crossover(ArrayList<PairOfChromosome> chromosomes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<PairOfChromosome> mutation(ArrayList<PairOfChromosome> chromosomes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Chromosome> replaceOldByNew(ArrayList<PairOfChromosome> chromosomes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
